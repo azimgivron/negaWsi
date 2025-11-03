55d9c1b3acb0bf2fdbc44cd3a12b91ae91161ed2
azimG <azimgivron@gmail.com>
2025-11-03 15:36:14 +0100

    Initialize negaWsi module with exports

---
+++ src/negaWsi/__init__.py
@@ -0,0 +1,11 @@
+"""Compute Module"""
+
+from negaWsi.nega import Nega
+from negaWsi.nega_fs import NegaFS
+from negaWsi.nega_reg import NegaReg
+
+__all__ = [
+    "Nega",
+    "NegaFS",
+    "NegaReg",
+]
