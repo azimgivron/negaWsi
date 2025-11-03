bf63ba6b9681367d25e418304449f3afe7ce536d
azimG <azimgivron@gmail.com>
2025-11-03 15:48:49 +0100

    Add Result import to __init__.py and update __all__

--- src/negaWsi/__init__.py
+++ src/negaWsi/__init__.py
@@ -3,9 +3,12 @@
 from negaWsi.nega import Nega
 from negaWsi.nega_fs import NegaFS
 from negaWsi.nega_reg import NegaReg
+from negaWsi.result import Result
 
 __all__ = [
     "Nega",
     "NegaFS",
     "NegaReg",
+    "Result"
 ]
+
